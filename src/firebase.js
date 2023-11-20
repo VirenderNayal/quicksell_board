ee88d5972d6a1da78b2a58cf73befbe90064b56e
Virender Nayal <virendernayal3003@gmai.com>
2023-11-20 19:33:58 +0530

    deployment

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCwCNgwUarubyVL5ithyoT8BGQsq8dhPyU",
+  authDomain: "quicksell-board.firebaseapp.com",
+  projectId: "quicksell-board",
+  storageBucket: "quicksell-board.appspot.com",
+  messagingSenderId: "1013091181389",
+  appId: "1:1013091181389:web:33e6811b22ee26d2b7cd04"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
